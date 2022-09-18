50ee7f4a5768813a4f0f59d78985c42f74afaa04
Tatjana <taninjawi@mail.ru>
2022-09-18 16:02:31 +0700

    Добавила заполнение нового массива

--- Program.cs
+++ Program.cs
@@ -20,6 +20,16 @@ for (int i = 0; i < array.Length; i++)
     }
 }
 string[] newArray = new string[count];
+count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[count] = array[i];
+        count++;
+    }
+}
+
 
 
 
