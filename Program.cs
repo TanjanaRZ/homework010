77ae8f8cbbc122df1040d28d7d2f859b0cc15027
Tatjana <taninjawi@mail.ru>
2022-09-18 14:25:58 +0700

    Ввела счётчик элементов меньше 3 символов

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует
+// новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
+//выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] array = { "Hello", "2", "world", ":-)" };
+int count = 0;
+string[] newArray = new string[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[count] = array[i];
+        count++;
+    }
+}
+for (int i = 0; i < count; i++)
+{
+    Console.Write(newArray[i] + " ");
+}
+Console.WriteLine();
+
+
+
+string[] array = { "Hello", "2", "world", ":-)" };
+int count = 0;
+//string[] newArray= new string [array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        count++;
+    }
+}
+string[] newArray = new string[count];
+count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[count] = array[i];
+        count++;
+    }
+}
+Console.WriteLine(string.Join(", " ,newArray));
+
+
+
+
